519dae413f65c641ff1ba797bf7107a9ed9659ed
Olamyy <olamyy53@gmail.com>
2019-09-14 20:38:08 +0100

    Fixes #1 #2 and part of #3.

--- cleaner.py
+++ cleaner.py
@@ -1,18 +1,26 @@
-import logging
 import os
 from pathlib import Path
 import bibtexparser
 import pandas
-from bibtexparser.bibdatabase import BibDatabase
+import yaml
+from bibtexparser.bibdatabase import BibDatabase, BibDataStringExpression, BibDataString
 from bibtexparser.bparser import BibTexParser
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.customization import type as bib_type
 from bibtexparser.customization import author, editor, journal, keyword, link, page_double_hyphen, doi, convert_to_unicode
 from tabulate import tabulate
-
 import logging
 
 logger = logging.getLogger('aclean')
+
+
+def _str_or_expr_to_bibtex(e):
+    if isinstance(e, BibDataStringExpression):
+        return ' # '.join([_str_or_expr_to_bibtex(s) for s in e.expr])
+    elif isinstance(e, BibDataString):
+        return e.name
+    else:
+        return '"{}"'.format(e)
 
 
 class Writer(BibTexWriter):
@@ -22,8 +30,34 @@ class Writer(BibTexWriter):
             try:
                 bibtex += getattr(self, '_' + content + '_to_bibtex')(bib_database)
             except AttributeError:
-                logger.warning("BibTeX item '{}' does not exist and will not be written. Valid items are {}."
-                               .format(content, self._valid_contents))
+                print("BibTeX item '{}' does not exist and will not be written. Valid items are {}.".format(content, self._valid_contents))
+        return bibtex
+
+    def _entry_to_bibtex(self, entry):
+        bibtex = ''
+        bibtex += '@' + entry['ENTRYTYPE'] + '{' + entry['ID']
+        display_order = [i for i in self.display_order if i in entry]
+        display_order += [i for i in sorted(entry) if i not in self.display_order]
+        if self.comma_first:
+            field_fmt = u"\n{indent}, {field:<{field_max_w}} = {value}"
+        else:
+            field_fmt = u",\n{indent}{field:<{field_max_w}} = {value}"
+        for field in [i for i in display_order if i not in ['ENTRYTYPE', 'ID']]:
+            try:
+                bibtex += field_fmt.format(
+                    indent=self.indent,
+                    field=field,
+                    field_max_w=self._max_field_width,
+                    value=_str_or_expr_to_bibtex(entry[field]))
+            except TypeError:
+                raise TypeError(u"The field %s in entry %s must be a string"
+                                % (field, entry['ID']))
+        if self.add_trailing_comma:
+            if self.comma_first:
+                bibtex += '\n' + self.indent + ','
+            else:
+                bibtex += ','
+        bibtex += "\n}\n" + self.entry_separator
         return bibtex
 
 
@@ -40,15 +74,19 @@ def customizations(record):
 
 
 class ACLCleaner(object):
-    def __init__(self, bibtex, output, keepkey=True):
+    def __init__(self, bibtex, output, keepkey=True, show_output=False, concise=False, write_to_file=False):
         self.bibtex = bibtex
         self.output = output
         self.keepkey = keepkey
-        self.anthology_path = list(Path(os.path.dirname(__file__)).joinpath('data').iterdir())[0]
+        self.show_ouput = show_output
+        self.concise = concise
+        self.write_to_file = write_to_file
+        self.anthology_path = list(Path(os.path.dirname(__file__)).joinpath('data').iterdir())[1]
+        self.venues_path = list(Path(os.path.dirname(__file__)).joinpath('data').iterdir())[0]
         self.bibdata = pandas.read_csv(self.anthology_path, compression='zip', low_memory=False)
 
     def match_title(self, value):
-        m = self.bibdata[self.bibdata.title.str.contains(value, regex=False)]
+        m = self.bibdata[self.bibdata.title.str.contains(value[:-1], regex=False)]
         return m
 
     def match_authors(self, value, inp=None):
@@ -92,16 +130,34 @@ class ACLCleaner(object):
         output = list(map(self.match, bibdata.entries))
         out.append(output)
 
-        report = [[i[1].get('title'), i[0]] for i in out[0]]
-        print(tabulate(report, headers=["Paper", "Match Found"], tablefmt="github"))
+        if self.show_ouput:
+            report = [[i[1].get('title'), i[0]] for i in out[0]]
+            print(tabulate(report, headers=["Paper", "Match Found"], tablefmt="github"))
 
         db = BibDatabase()
         bibcontent = [i[1] for i in out[0]]
         with open(self.output, 'w') as bibfile:
             for bib in bibcontent:
-                db.entries = [bib]
+                cleaned_bib = {k: v for k, v in bib.items() if len(v) != 1}
+
+                if self.concise:
+                    cleaned_bib['publisher'] = self.get_publish()
+                    to_remove = ["abstracts"]
+                    cleaned_bib = {k: v for k, v in cleaned_bib.items() if k not in to_remove}
+                print(cleaned_bib)
+                db.entries = [cleaned_bib]
                 writer = Writer()
+                writer.order_entries_by = ('ID', 'title', 'author', 'booktitle', 'month', 'year', 'address', 'publisher', 'url', 'pages')
                 writer.indent = '  '
                 writer.align_values = True
-                bibfile.write(writer.write(db))
+                if self.write_to_file:
+                    bibfile.write(writer.write(db))
+                else:
+                    print(writer.write(db))
         return True
+
+    def get_publish(self):
+        with open(self.venues_path, 'r') as f:
+            venues = yaml.load(f)
+        return True
+
--- script.py
+++ script.py
@@ -8,9 +8,12 @@ from cleaner import ACLCleaner
 @click.argument('bibtex', type=click.Path())
 @click.option('--output', type=click.Path())
 @click.option('--keepkey', type=click.INT, default=1, help='Keep current key')
-def aclbibcleaner(bibtex, output, keepkey):
+@click.option('--concise', type=click.INT, default=1, help='Do deep cleaning.')
+@click.option('--show_output', type=click.INT, default=0, help="Show cleaning report in a table")
+@click.option('--write_to_file', type=click.INT, default=0, help="Write output to file")
+def aclbibcleaner(bibtex, output, keepkey, concise, show_output, write_to_file):
     output = output if output else "cleaned_bib.bib"
-    cleaner = ACLCleaner(bibtex, output, keepkey)
+    cleaner = ACLCleaner(bibtex, output, keepkey, concise=concise, show_output=show_output, write_to_file=write_to_file)
     return cleaner.clean()
 
 
